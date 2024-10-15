d3c0a8c8177c0ef127e6530bb6f0b6b2260e8cb1
TheBillPlease <ec21631@qmul.ac.uk>
2024-10-15 17:58:48 +0100

    Bookshelf Scene, Tested GetKey input functions
    Added a new scene for the Bookshelf puzzle with a plane in it.
    Started testing different ways to change the scene when a key is pressed and the player is in a certain area. - Unsuccessful at the moment

--- Assets/Scripts/TriggerInteraction.cs
+++ Assets/Scripts/TriggerInteraction.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerInteraction : MonoBehaviour
 {
@@ -10,11 +12,26 @@ public class TriggerInteraction : MonoBehaviour
     public Material highlightMaterial; // highlight material when player is near object
     public Material defaultMaterial;
     public TMP_Text objectText;
+    public GameObject scene;
+
+    private bool bookshelfCollision;
+   
 
 
-    private void Start()
+    void Start()
     {
+        bookshelfCollision = false;
+        Debug.Log("bool is false");
         objectText.text = string.Empty;
+    }
+
+    void Update()
+    {
+        if (bookshelfCollision && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Key Pressed and in space");
+            SceneManager.LoadScene((scene.ToString()));
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -26,6 +43,10 @@ public class TriggerInteraction : MonoBehaviour
             if (objectText.name == "BookshelfText")
             {
                 objectText.text = "Bookshelf. Press 'E' to interact";
+
+                Debug.Log("Bool is true");
+                 bookshelfCollision = true;
+
             }
             else if (objectText.name == "PaintingText")
             {
