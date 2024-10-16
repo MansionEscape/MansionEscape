464b0019c7fd6c997448768797aa81e5a9ce82e3
Khadijah Darragi <khadijahdarragi2003@gmail.com>
2024-10-16 20:14:14 +0100

    prompts now work and puzzle panel displays for chest object

--- Assets/Scripts/TriggerInteraction.cs
+++ Assets/Scripts/TriggerInteraction.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 public class TriggerInteraction : MonoBehaviour
@@ -10,15 +13,19 @@ public class TriggerInteraction : MonoBehaviour
     public Material highlightMaterial; // highlight material when player is near object
     public Material defaultMaterial;
     public TMP_Text objectText; // text for interaction prompts
-
+    public string interactionMessage; // custom message to display
+    public GameObject puzzlePanel; // UI panel of the puzzle
+    private bool isPlayerNear = false; // to check if player is in range
 
     private void Start()
     {
         objectText.text = string.Empty;
+        puzzlePanel.SetActive(false);        // ensure panel is hidden at start
     }
 
     void OnTriggerEnter(Collider other)
     {
+        /*
         if (other.gameObject == player)
         {
             interactiveObject.GetComponent<Renderer>().material = highlightMaterial;
@@ -35,8 +42,14 @@ public class TriggerInteraction : MonoBehaviour
             {
                 objectText.text = "Chest. Press 'E' to interact";
             }
-            
+        }          // replaced this code with the code below as it is shorter
+        */
 
+        if (other.gameObject == player) 
+        {
+            interactiveObject.GetComponent<Renderer>().material = highlightMaterial;
+            objectText.text = interactionMessage;
+            isPlayerNear = true;    
         }
     }
 
@@ -46,6 +59,17 @@ public class TriggerInteraction : MonoBehaviour
         {
             interactiveObject.GetComponent<Renderer>().material = defaultMaterial;
             objectText.text = string.Empty;
+            isPlayerNear = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E)) // check if player is near and presses 'E'
+        {
+            Debug.Log("Interaction with object: " + interactiveObject.name); // to check if prompt is working 
+            puzzlePanel.SetActive(true);   // display the puzzle panel
+
         }
     }
 }
