1fef57b7ad4c2b166633bce9526c1c62c3aca4da
Khadijah Darragi <khadijahdarragi2003@gmail.com>
2024-10-17 17:09:45 +0100

    updated trigger script for UI panels
    updated trigger script - added 2 variables and an update function which checks if key prompt E is entered and if yes, then displays the puzzle panel parameter.

--- Assets/Scripts/TriggerInteraction.cs
+++ Assets/Scripts/TriggerInteraction.cs
@@ -13,12 +13,15 @@ public class TriggerInteraction : MonoBehaviour
     public Material defaultMaterial;
     public TMP_Text objectText; // text for interaction prompts
     public string interactionMessage; // custom prompt message to display
+    public GameObject puzzlePanel; // UI panel of the puzzle
+    private bool isPlayerNear = false; // to check if player is in range
 
 
 
     private void Start()
     {
         objectText.text = string.Empty;
+        puzzlePanel.SetActive(false);      // ensure panel is hidden at start
     }
 
     void OnTriggerEnter(Collider other)
@@ -27,6 +30,7 @@ public class TriggerInteraction : MonoBehaviour
         {
             interactiveObject.GetComponent<Renderer>().material = highlightMaterial;
             objectText.text = interactionMessage;   // display the custom message prompt
+            isPlayerNear = true;
         }
     }
 
@@ -36,6 +40,19 @@ public class TriggerInteraction : MonoBehaviour
         {
             interactiveObject.GetComponent<Renderer>().material = defaultMaterial;
             objectText.text = string.Empty;
+            isPlayerNear = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Interaction with object: " + interactiveObject.name); // check if prompt is working
+            puzzlePanel.SetActive(true); // display the UI puzzle panel
+
+            // Add code here for changing scene if puzzle is on a scene instead of UI.
+
         }
     }
 }
