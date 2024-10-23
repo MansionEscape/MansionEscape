9cf18f946571e23c12545968ef11fcb17cd89749
Khadijah Darragi <khadijahdarragi2003@gmail.com>
2024-10-23 09:06:03 +0100

    Build Project
    updated projectsettings and got rid of errors causing build to fail - build now completed

--- Assets/Scripts/PlayerInput/TriggerInteraction.cs
+++ Assets/Scripts/PlayerInput/TriggerInteraction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.IMGUI.Controls;
+//using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
