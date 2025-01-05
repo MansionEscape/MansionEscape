c6163cc72f127737a961ccd93bb856edfd7f7108
Alesha <119981670+AleshaTasnim@users.noreply.github.com>
2025-01-05 03:23:38 +0000

    Items to pick up in kitchen set

---
+++ Assets/Scripts/Kitchen/BeerBarrelInteraction.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeerBarrelInteraction : MonoBehaviour
+{
+    public PlayerManager player; 
+    public MainController controller; 
+    public Item mugItem; 
+    public Item beerItem; 
+    public AudioClip pourSound; 
+    private AudioSource audioSource; 
+    private bool isPlayerNearby = false; 
+
+    public Material highlightMaterial; 
+    private Material originalMaterial;
+    private Renderer barrelRenderer;
+
+    private void Start()
+    {
+        player = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
+        controller = GameObject.Find("MainGameController").GetComponent<MainController>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        barrelRenderer = GetComponent<Renderer>();
+        if (barrelRenderer != null)
+        {
+            originalMaterial = barrelRenderer.material;
+        }
+    }
+
+    private void Update()
+    {
+  
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            if (PlayerHasItem(mugItem))
+            {
+                Interact();
+            }
+            else
+            {
+                controller.instructionBox.SetActive(true);
+                controller.instructionText.text = "You need a mug to get beer.";
+            }
+        }
+    }
+
+    private void Interact()
+    {
+        // Add beer to the inventory
+        InventoryManager.Instance.ObjectPickedUp(beerItem);
+
+        // Play the sound effect
+        if (pourSound != null)
+        {
+            audioSource.PlayOneShot(pourSound);
+        }
+
+        controller.instructionBox.SetActive(false);
+        controller.instructionText.text = "You got beer!";
+        Debug.Log("Beer collected!");
+    }
+
+    private bool PlayerHasItem(Item requiredItem)
+    {
+        foreach (var item in player.data.items)
+        {
+            if (item == requiredItem)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+            controller.instructionBox.SetActive(true);
+            controller.instructionText.text = "Press 'E' to interact with the beer barrel.";
+
+            // Highlight the barrel
+            if (barrelRenderer != null && highlightMaterial != null)
+            {
+                barrelRenderer.material = highlightMaterial;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+            controller.instructionBox.SetActive(false);
+            controller.instructionText.text = "";
+
+            // Revert barrel material to original
+            if (barrelRenderer != null && originalMaterial != null)
+            {
+                barrelRenderer.material = originalMaterial;
+            }
+        }
+    }
+}
