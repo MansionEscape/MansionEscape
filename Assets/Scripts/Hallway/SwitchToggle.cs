cd17a9d14ae176aac66f232022e23cb3b214a1c6
Khadijah Darragi <khadijahdarragi2003@gmail.com>
2025-01-06 06:34:14 +0000

    Hallway Light Switch Scene
    changed scene to match new input system + got rid of old hallway scenes & files.

--- Assets/Scripts/Hallway/SwitchToggle.cs
+++ Assets/Scripts/Hallway/SwitchToggle.cs
@@ -1,35 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SwitchToggle : MonoBehaviour
 {
-    private Animator switchAnimator;
-    public GameObject Light;
+    private Animator switchAnimator;       // Animator for the switch
+    public GameObject Light;              // Reference to the Light GameObject
+
+    private Camera mainCamera;            // Reference to the main camera
+    public InputActionAsset inputActions; // Reference to the Input Action Asset
+    private InputAction mouseClickAction; // Mouse click action
 
     private void Awake()
     {
         // Get Animator component attached to this GameObject
         switchAnimator = GetComponent<Animator>();
 
-        // If IsOn parameter is false, automatically set light to false
+        // Ensure Light starts in the correct state
         if (!switchAnimator.GetBool("IsOn"))
         {
             Light.SetActive(false);
         }
+
+        // Get main camera
+        mainCamera = Camera.main;
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        // Check for input
-        if (Input.GetKeyDown(KeyCode.E))
+        // Find the Mouse Left Click action in the Input Action Asset
+        var actionMap = inputActions.FindActionMap("Mouse");
+        mouseClickAction = actionMap.FindAction("MouseLeftClick");
+
+        // Subscribe to the mouse click action
+        mouseClickAction.performed += OnMouseClick;
+        mouseClickAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from the mouse click action
+        mouseClickAction.performed -= OnMouseClick;
+        mouseClickAction.Disable();
+    }
+
+    private void OnMouseClick(InputAction.CallbackContext context)
+    {
+        // Cast a ray from the mouse position
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Ray ray = mainCamera.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            ToggleSwitch();
+            // Check if the clicked object is this switch
+            if (hit.collider.gameObject == gameObject)
+            {
+                ToggleSwitch();
+            }
         }
-    }
-
-    private void OnMouseDown()
-    {
-        // Toggle switch on mouse click
-        ToggleSwitch();
     }
 
     private void ToggleSwitch()
@@ -41,13 +68,6 @@ public class SwitchToggle : MonoBehaviour
         switchAnimator.SetBool("IsOn", !isOn);
 
         // Toggle Light based on IsOn value
-        if (!isOn)
-        {
-            Light.SetActive(true);
-        }
-        else
-        {
-            Light.SetActive(false);
-        }
+        Light.SetActive(!isOn);
     }
 }
